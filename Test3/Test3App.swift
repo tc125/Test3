8c43286378d9d64cebdac7b6383a4033e0cf094d
Tonya <failjam0125@gmail.com>
2023-01-05 13:30:21 +0800

    Initial Commit

---
+++ Test3/Test3App.swift
@@ -0,0 +1,17 @@
+//
+//  Test3App.swift
+//  Test3
+//
+//  Created by SHI WING FUNG on 5/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Test3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
